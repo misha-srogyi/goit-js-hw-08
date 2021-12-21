6381e863e1b23efb1b498a0a344121beb8cf16a2
misha-srogyi <srogyj@gmail.com>
2021-12-21 21:09:52 +0100

    all new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// // Change code below this line
 
-// console.log(galleryItems);
+// // console.log(galleryItems);
 
-const galleryRefs = document.querySelector('.gallery');
-const galleryMarkup = createGalleryMarkup(galleryItems);
+// const galleryRefs = document.querySelector('.gallery');
+// const galleryMarkup = createGalleryMarkup(galleryItems);
 
-galleryRefs.insertAdjacentHTML('beforeend', galleryMarkup);
+// galleryRefs.insertAdjacentHTML('beforeend', galleryMarkup);
 
-function createGalleryMarkup(galleryItems) {
-    return galleryItems
-        .map(({ preview, original, description }) => {
-        return `
-        <a class="gallery__item" href="${original}">
-    <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-        `;
-    }).join('');
-}
+// function createGalleryMarkup(galleryItems) {
+//     return galleryItems
+//         .map(({ preview, original, description }) => {
+//         return `
+//         <a class="gallery__item" href="${original}">
+//     <img class="gallery__image" src="${preview}" alt="${description}" />
+//         </a>
+//         `;
+//     }).join('');
+// }
 
-let gallery = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    captionPosition: 'bottom',
-    }
-);
+// let gallery = new SimpleLightbox('.gallery a', {
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//     captionPosition: 'bottom',
+//     }
+// );
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,12 @@
 
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
 
-player.on('play', function() {
-        console.log('played the video!');
-    });
+// player.on('play', function() {
+//         console.log('played the video!');
+//     });
 
 
