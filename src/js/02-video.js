03628e589bc65c7cb4ad3d366f4e9c09f152cbe5
misha-srogyi <srogyj@gmail.com>
2021-12-21 17:06:47 +0100

    add files

---
+++ src/js/02-video.js
@@ -0,0 +1,12 @@
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+        console.log('played the video!');
+    });
+
+
