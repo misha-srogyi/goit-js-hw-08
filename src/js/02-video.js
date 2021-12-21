34b975eb0df520ccf988a3464b497c4900f8102d
misha-srogyi <srogyj@gmail.com>
2021-12-22 00:40:20 +0100

    add 1-2 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,24 @@
 
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-
-// player.on('play', function() {
-//         console.log('played the video!');
-//     });
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+function onPlay (evt) {
+    const seconds = evt.seconds;
+    localStorage.setItem('current-time', seconds)
+};
+
+setCurrentTime();
+
+function setCurrentTime() {
+    const savedTime = localStorage.getItem('current-time');
+    if (savedTime) {
+        player.setCurrentTime(savedTime);
+    }
+};
