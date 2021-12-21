ea5508fd7c6792247d8db75ccf718dbe76d85cb9
misha-srogyi <srogyj@gmail.com>
2021-12-21 23:22:30 +0100

    comlete parcel

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,9 +2,9 @@
 import { galleryItems } from './gallery-items';
 // import SimpleLightbox from 'simplelightbox';
 // import 'simplelightbox/dist/simple-lightbox.min.css';
-// // Change code below this line
+// Change code below this line
 
-// // console.log(galleryItems);
+console.log(galleryItems);
 
 // const galleryRefs = document.querySelector('.gallery');
 // const galleryMarkup = createGalleryMarkup(galleryItems);
