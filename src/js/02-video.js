56c8bad17bec47f19aa14418db02beac57fc663e
misha-srogyi <srogyj@gmail.com>
2021-12-23 03:20:56 +0100

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,20 +4,20 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-
+const KEY_CURRENT_TIME = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 
 function onPlay (evt) {
     const seconds = evt.seconds;
-    localStorage.setItem('current-time', seconds)
+    localStorage.setItem(KEY_CURRENT_TIME, seconds)
 };
 
 setCurrentTime();
 
 function setCurrentTime() {
-    const savedTime = localStorage.getItem('current-time');
+    const savedTime = localStorage.getItem(KEY_CURRENT_TIME);
     if (savedTime) {
         player.setCurrentTime(savedTime);
     }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
 import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-form-state";
+let formObject = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('.feedback-form input'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+
+getValue();
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function onInput(evt) {
+    // console.log(evt.target.name);
+    // console.log(evt.target.value);
+    formObject[evt.target.name] = evt.target.value;
+    console.log(formObject);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formObject));
+    
+};
+
+function getValue() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const parsedMessage = JSON.parse(savedMessage);
+
+    if (parsedMessage) {
+        refs.input.value = parsedMessage.email || '';
+        formObject.email = refs.input.value;
+
+        refs.textarea.value = parsedMessage.message || '';
+        formObject.message = refs.textarea.value;
+    }
+
+}
